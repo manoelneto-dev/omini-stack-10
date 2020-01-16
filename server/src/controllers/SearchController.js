bf43d55d72ca078f07cb769d8a004a6039ff5dfa
Manoel Neto <manoelneto.dev@gmail.com>
2020-01-16 20:39:41 -0300

    Finishing backend routes

--- server/src/controllers/SearchController.js
+++ server/src/controllers/SearchController.js
@@ -1,9 +1,27 @@
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/ParseStringAsArray')
 
 module.exports = {
     async index(request, response) {
-        console.log(request.query)
+        const { latitude, longitude, techs } = request.query
 
-        return response.json({ devs: [] })
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        })
+
+        return response.json({ devs })
     },
 }
